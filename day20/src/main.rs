fc8199036fad6f987ecfa7d8c1c0ad0a732f6bf0
Adrien Faure <adrien.faure@protonmail.com>
2025-01-05 20:47:03 +0100

    day-20: p2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -14,11 +14,7 @@ fn main() -> std::io::Result<()> {
     let res = file
         .clone()
         .into_iter()
-        .fold(file.clone(), |mut acc, (idx, mut n)| {
-            // print!(
-            //     "acc (move {n:>2}): {:?}",
-            //     acc.iter().map(|e| e.1).collect_vec()
-            // );
+        .fold(file.clone(), |mut acc, (idx, n)| {
             let pos = acc.iter().position(|e| idx == e.0).unwrap();
 
             acc.remove(pos);
@@ -26,20 +22,7 @@ fn main() -> std::io::Result<()> {
             let new_idx = (pos as i64 + n).rem_euclid(acc.len() as i64);
             acc.insert(new_idx as usize, (idx, n));
 
-            // println!(" ==> {:?}", acc.iter().map(|e| e.1).collect_vec());
             acc
-
-            // acc.into_iter()
-            //     .map(|(old_idx, n)| {
-            //         if idx > old_idx {
-            //             (idx - n, n)
-            //         } else if idx < old_idx {
-            //             (idx + n, n)
-            //         } else {
-            //             (idx, n)
-            //         }
-            //     })
-            //     .collect_vec()
         });
 
     assert!(res.iter().any(|(_idx, n)| *n == 0));
@@ -57,5 +40,38 @@ fn main() -> std::io::Result<()> {
 
     println!("p1: {:?}", p1);
 
+    let magic_number = 811589153;
+    let file_with_key = file.iter().map(|e| (e.0, e.1 * magic_number)).collect_vec();
+
+    let res = file_with_key
+        .clone()
+        .into_iter()
+        .cycle()
+        .take(file_with_key.len() * 10)
+        .enumerate()
+        .fold(file_with_key.clone(), |mut acc, (debug, (idx, n))| {
+            let pos = acc.iter().position(|e| idx == e.0).unwrap();
+
+            acc.remove(pos);
+
+            let new_idx = (pos as i64 + n).rem_euclid(acc.len() as i64);
+            acc.insert(new_idx as usize, (idx, n));
+            acc
+        });
+
+    assert!(res.iter().any(|(_idx, n)| *n == 0));
+
+    let p2 = res
+        .into_iter()
+        .cycle()
+        .map(|e| e.1)
+        .skip_while(|e| *e != 0)
+        .enumerate()
+        .filter(|(idx, _)| [1000, 2000, 3000].contains(idx))
+        .take(3)
+        .map(|e| e.1)
+        .sum::<i64>();
+
+    println!("p2: {:?}", p2);
     Ok(())
 }
